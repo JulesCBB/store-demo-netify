0c959ad2e21638a396e3081a06cd6e2a25908a9f
julie <jcbb009@gmail.com>
2020-07-22 18:35:34 +0200

    Updates

---
+++ precache-manifest.e409a63010dd97ec45e7eea309f7a3b9.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a560297fa8cef73eeb8c4a584198d02",
+    "url": "/store-demo/index.html"
+  },
+  {
+    "revision": "3b6f7ed950310f3f2f0b",
+    "url": "/store-demo/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "469c447a293e9fa71f00",
+    "url": "/store-demo/static/css/main.ae166f54.chunk.css"
+  },
+  {
+    "revision": "3b6f7ed950310f3f2f0b",
+    "url": "/store-demo/static/js/2.4b46e4ae.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/store-demo/static/js/2.4b46e4ae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "469c447a293e9fa71f00",
+    "url": "/store-demo/static/js/main.7a41fb89.chunk.js"
+  },
+  {
+    "revision": "6b76efc64b7ff93de0bc",
+    "url": "/store-demo/static/js/runtime-main.c232e393.js"
+  },
+  {
+    "revision": "4e33d483b5e8ae71b409d821715119bd",
+    "url": "/store-demo/static/media/logo.4e33d483.svg"
+  }
+]);
